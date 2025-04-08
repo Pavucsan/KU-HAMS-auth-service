0da124347577585d790b6b1bf170b90a73a2e639
Pavucs Manokar <pavucsan@smartzi.com>
2025-04-09 00:14:25 +0530

    project init

---
+++ src/main/java/com/ku/kuhamsauthservice/KuHamsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ku.kuhamsauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuHamsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuHamsAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ku/kuhamsauthservice/KuHamsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ku.kuhamsauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuHamsAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
